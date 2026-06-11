f7b7398b376f657e0bcc93cc73ebefb243939718
pritanshi-gupta <guptapritanshi@gmail.com>
2026-06-11 16:48:12 +0530

    Add Music feature with routing, component, template, styles, and tests

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MusicComponent } from './Features/music/music.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'music', pathMatch: 'full' },
+  { path: 'music', component: MusicComponent },
+
+  // future pages (example)
+  // { path: 'home', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
