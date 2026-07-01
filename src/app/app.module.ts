7cbf0b932081db255490153eed4ab60960c6df9c
pritanshi-gupta <guptapritanshi@gmail.com>
2026-07-01 10:00:45 +0530

    replace all UI emojis and symbols with Angular Material mat-icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +21,8 @@ import { SetRbtComponent } from './Features/set-rbt/set-rbt.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatDialogModule,
-     HttpClientModule
+     HttpClientModule,
+     MatIconModule
   ],
 
   providers: [],
